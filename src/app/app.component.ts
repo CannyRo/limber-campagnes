1789bbad9439262acd996b3c9a557972a7391c41
CannyRo <whatcannydev@gmail.com>
2024-09-19 14:28:20 +0200

    Static table with all the data, need to use a getter with page number and number of item by page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, NavbarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/services/campaign.service.ts
+++ src/app/shared/services/campaign.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Campaign, CampaignList, RawCampaign } from '../models/campaign.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CampaignService {
+  private readonly http = inject(HttpClient);
+  private readonly DATA_API_URL = 'http://localhost:3000/campaigns';
 
-  constructor() { }
+  getCampaignList(): Observable<CampaignList> {
+    return this.http.get<CampaignList>(this.DATA_API_URL);
+  }
+
+  // getCampaignListWithPage(page:number, numberPerPage:number): Observable<RawCampaign> {
+  //   return this.http.get<RawCampaign>(`${this.DATA_API_URL}?=${page}&_per_page=${numberPerPage}`).pipe(
+  //     map(res => res),
+  //     catchError(this.handleError<any>('getCampaign Error'))
+  //   );
+  // }
+  getCampaignListWithPage(page:number, numberPerPage:number): Observable<RawCampaign> {
+    return this.http.get<RawCampaign>(`${this.DATA_API_URL}?=${page}&_per_page=${numberPerPage}`);
+  }
+
+  getCampaingById(id:number): Observable<Campaign> {
+    console.log(this.http.get<Campaign>(`${this.DATA_API_URL}/${id}`));
+    return this.http.get<Campaign>(`${this.DATA_API_URL}/${id}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
